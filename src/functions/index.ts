9932c3cd26434a43307523afefa8df82aff37c16
Raymond <ray@firestitch.com>
2018-04-18 14:10:15 -0400

    added export

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -1,5 +1,7 @@
 export * from './ago';
 export * from './iso8601';
+export * from './duration';
 export * from './format';
 export * from './range';
 export * from './granularduration';
+export * from './tomomentformat';
