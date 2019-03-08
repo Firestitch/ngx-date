c2f9d8d115079a540b132b4d19ec176f8da79fc8
Raymond Gigliotti <ray@firestitch.com>
2019-03-07 22:32:36 -0500

    format() function can handle date strings

--- src/libs/format.ts
+++ src/libs/format.ts
@@ -1,9 +1,17 @@
-import { format as fnsFormat } from 'date-fns';
+import { format as fnsFormat, parseISO, isValid } from 'date-fns';
 import { getFormatString } from './get-format-string';
 
 export function format(date, formatTo?: string): string {
 
     const outputFormat = getFormatString(date, formatTo);
+
+    if (typeof date === 'string') {
+        date = parseISO(date);
+
+        if (!isValid(date)) {
+            return '';
+        }
+    }
 
     return fnsFormat(date, outputFormat);
 }
