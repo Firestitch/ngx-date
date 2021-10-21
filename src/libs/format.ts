f894dca93516f14cb2da67a42d4b2323d8d61004
Ray <ray@email.com>
2021-10-21 11:09:08 -0400

    FC-T366_FsDatePicker:_timezone

--- src/libs/format.ts
+++ src/libs/format.ts
@@ -1,10 +1,10 @@
-import { format as fnsFormat } from 'date-fns';
+import { format as fnsFormat } from 'date-fns-tz';
 import { getFormatString } from './get-format-string';
 import { parse } from './parse';
 
-export function format(date, formatTo?: string): string {
+export function format(date, formatTo?: string, options: { timezone?: string } = {}): string {
     date = parse(date);
     const outputFormat = getFormatString(formatTo, date);
 
-    return date ? fnsFormat(date, outputFormat) : null;
+    return date ? fnsFormat(date, outputFormat, { timeZone: options.timezone }) : null;
 }
