3d5f817ba924718fac3bbddecc83c1703adcb5e4
Raymond Gigliotti <ray@firestitch.com>
2023-05-30 14:06:17 -0400

    changed poppover to mattooltip

--- src/app/fs-date.module.ts
+++ src/app/fs-date.module.ts
@@ -2,7 +2,6 @@ import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { FsCommonModule } from '@firestitch/common';
-import { FsPopoverModule } from '@firestitch/popover';
 
 import { FsDatePipe } from './pipes/date.pipe';
 import { FsDateRangePipe } from './pipes/date-range.pipe';
@@ -15,13 +14,16 @@ import { FsDateAgoDirective } from './directives/fs-date-ago.directive';
 import { FsDateAgoComponent } from './components/date-ago/date-ago.component';
 
 import { FsDateAgoFactory } from './services/date-ago-factory.service';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 
 @NgModule({
   imports: [
     CommonModule,
+
+    MatTooltipModule,
+
     FsCommonModule,
-    FsPopoverModule
   ],
   exports: [
     FsDatePipe,
@@ -29,9 +31,7 @@ import { FsDateAgoFactory } from './services/date-ago-factory.service';
     FsDateDurationPipe,
     FsDateFormatPipe,
     FsDateRangePipe,
-
     FsDateAgoDirective,
-
     FsDateAgoComponent
   ],
   entryComponents: [
