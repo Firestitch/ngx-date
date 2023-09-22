e6ba54e884ed4e1d3731aeeffd32377d50f72616
Raymond Gigliotti <ray@firestitch.com>
2023-09-22 11:15:12 -0400

    npm cleanup

--- playground/app/playground.module.ts
+++ playground/app/playground.module.ts
@@ -1,32 +1,29 @@
 // Angular
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
 
 // Material
 import { AppMaterialModule } from './material.module';
 
 // FireStitch
-import { FsExampleModule } from '@firestitch/example';
 import { FsDateModule } from '@firestitch/date';
+import { FsExampleModule } from '@firestitch/example';
 import { FsMessageModule } from '@firestitch/message';
 
-import { ToastrModule } from 'ngx-toastr';
-
-// Current module
 import { AppComponent } from './app.component';
-import { FormatExampleComponent } from './components/format-example/format-example.component';
 import { AgoExampleComponent } from './components/ago-example/ago-example.component';
-import { DurationExampleComponent } from './components/duration-example/duration-example.component';
-import { RangeExampleComponent } from './components/range-example/range-example.component';
 import { DateExampleComponent } from './components/date-example/date-example.component';
+import { DurationExampleComponent } from './components/duration-example/duration-example.component';
+import { FormatExampleComponent } from './components/format-example/format-example.component';
+import { RangeExampleComponent } from './components/range-example/range-example.component';
 import { SafeHtmlPipe } from './components/safe-html/safe-html.pipe';
 
 
 @NgModule({
-  bootstrap: [ AppComponent ],
+  bootstrap: [AppComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
@@ -38,7 +35,6 @@ import { SafeHtmlPipe } from './components/safe-html/safe-html.pipe';
 
     FsExampleModule.forRoot(),
     FsMessageModule.forRoot(),
-    ToastrModule.forRoot({ preventDuplicates: true }),
   ],
   entryComponents: [
   ],
