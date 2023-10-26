63a31d4c60a85d6344b194ca033a6c6f5c01f609
Anatoly Thernytskyi <mendor1990@gmail.com>
2023-10-26 15:11:06 +0300

    optional options for ago pipe

--- src/libs/ago.ts
+++ src/libs/ago.ts
@@ -1,36 +1,37 @@
 import { differenceInMinutes, differenceInSeconds } from 'date-fns';
 
-import { format } from './format';
-import { duration } from './duration';
-import { parse } from './parse';
 import { round } from '@firestitch/common';
+import { duration } from './duration';
+import { format } from './format';
+import { parse } from './parse';
 
 
-export function ago(date, frmt = 'date') {
+export function ago(date, frmt = 'date', options: any = {}) {
 
-    date = parse(date);
-    if (!date) {
-      return '';
+  date = parse(date);
+  if (!date) {
+    return '';
+  }
+
+  const minDiff = round(differenceInSeconds(new Date(), date) / 60, 0);
+  const hourDiff = round(differenceInMinutes(new Date(), date) / 60, 0);
+
+  if (Math.abs(hourDiff) >= 24) {
+    if (date.getFullYear() == new Date().getFullYear()) {
+      return format(date, 'date-yearless');
+    } else {
+      return format(date, frmt);
     }
 
-    const minDiff = round(differenceInSeconds(new Date(), date) / 60, 0);
-    const hourDiff = round(differenceInMinutes(new Date(), date) / 60, 0);
-
-    if (Math.abs(hourDiff) >= 24) {
-      if (date.getFullYear() == new Date().getFullYear()) {
-        return format(date, 'date-yearless');
-      } else {
-        return format(date, frmt);
-      }
-
-    } else if (hourDiff == 0 && minDiff == 0) {
-      return 'now';
-     } else {
-      return duration(minDiff, {
-        unit: 'minutes',
-        suffix: true,
-        minutes: true,
-        hours: true
-      });
-    }
+  } else if (hourDiff == 0 && minDiff == 0) {
+    return 'now';
+  } else {
+    return duration(minDiff, {
+      unit: 'minutes',
+      suffix: true,
+      minutes: true,
+      hours: true,
+      ...options,
+    });
+  }
 }
